1d931c02bae17163cd3808dd407360af5f43597b
BrunoSilvaFreire <brunorbsf@gmail.com>
2020-03-04 08:19:43 -0300

    > Update DJ plugin namespace > Add plugin.json for DJ plugin

--- dj/src/main/kotlin/me/zortex/dj/DJ.kt
+++
@@ -1,8 +0,0 @@
-package me.zortex.dj
-
-import me.ddevil.mirai.plugins.Plugin;
-
-class DJ :Plugin(){
-    override fun bootstrap(){
-    }
-}
---
+++ dj/src/main/kotlin/me/zortex/mirai/dj/DJ.kt
@@ -0,0 +1,8 @@
+package me.zortex.mirai.dj
+
+import me.ddevil.mirai.plugins.Plugin;
+
+class DJ : Plugin() {
+    override fun bootstrap() {
+    }
+}

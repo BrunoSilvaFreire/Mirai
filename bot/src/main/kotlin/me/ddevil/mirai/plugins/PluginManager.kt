cbee1df8c1c30288153eda3cfe53566fd8cf3590
BrunoSilvaFreire <brunorbsf@gmail.com>
2020-03-04 08:38:31 -0300

    > Catching error while loading plugins

--- bot/src/main/kotlin/me/ddevil/mirai/plugins/PluginManager.kt
+++ bot/src/main/kotlin/me/ddevil/mirai/plugins/PluginManager.kt
@@ -41,40 +41,44 @@ class PluginManager(
         if (files != null) {
             logger.info("Found ${files.size} files.")
             for (listFile in files) {
-                val file = JarFile(listFile)
-                var entry = file.getJarEntry("plugin.json")
-                if (entry == null) {
-                    logger.warning("Jar file $file is not a plugin!")
-                    continue
+                try {
+                    val file = JarFile(listFile)
+                    var entry = file.getJarEntry("plugin.json")
+                    if (entry == null) {
+                        logger.warning("Jar file $file is not a plugin!")
+                        continue
+                    }
+                    val descriptor = PluginDescriptor.from(file.getInputStream(entry))
+                    val child = URLClassLoader(
+                        arrayOf<URL>(listFile.toURI().toURL()),
+                        this::class.java.classLoader
+                    )
+                    val classToLoad = Class.forName(descriptor.main, true, child)
+                    if (classToLoad == null) {
+                        logger.warning("Plugin ${descriptor.name}'s main class ${descriptor.main} could not be found.")
+                        continue
+                    }
+                    if (classToLoad.isAssignableFrom(Plugin::class.java)) {
+                        logger.warning("Plugin ${descriptor.name}'s main class ${descriptor.main} does not extend Plugin.")
+                        continue
+                    }
+                    val ctor = classToLoad.getConstructor()
+                    if (ctor == null) {
+                        logger.warning("Plugin ${descriptor.name}'s main class ${descriptor.main} does not have a no args constructor.")
+                        continue
+                    }
+                    val instance = ctor.newInstance()
+                    if (instance !is Plugin) {
+                        logger.severe("Plugin ${descriptor.name}'s instantiation of ${descriptor.main} does not extend Plugin.")
+                        continue
+                    }
+                    logger.info("Initialzing $instance")
+                    instance.initialize(descriptor, mirai)
+                    logger.info("Plugin ${descriptor.identifier} (${descriptor.name}) loaded.")
+                    loadedPlugins += instance
+                } catch (exception: Throwable) {
+                    logger.warning("Error while loading plugin ${listFile}, skipping.")
                 }
-                val descriptor = PluginDescriptor.from(file.getInputStream(entry))
-                val child = URLClassLoader(
-                    arrayOf<URL>(listFile.toURI().toURL()),
-                    this::class.java.classLoader
-                )
-                val classToLoad = Class.forName(descriptor.main, true, child)
-                if (classToLoad == null) {
-                    logger.warning("Plugin ${descriptor.name}'s main class ${descriptor.main} could not be found.")
-                    continue
-                }
-                if (classToLoad.isAssignableFrom(Plugin::class.java)) {
-                    logger.warning("Plugin ${descriptor.name}'s main class ${descriptor.main} does not extend Plugin.")
-                    continue
-                }
-                val ctor = classToLoad.getConstructor()
-                if (ctor == null) {
-                    logger.warning("Plugin ${descriptor.name}'s main class ${descriptor.main} does not have a no args constructor.")
-                    continue
-                }
-                val instance = ctor.newInstance()
-                if (instance !is Plugin) {
-                    logger.severe("Plugin ${descriptor.name}'s instantiation of ${descriptor.main} does not extend Plugin.")
-                    continue
-                }
-                logger.info("Initialzing $instance")
-                instance.initialize(descriptor, mirai)
-                logger.info("Plugin ${descriptor.identifier} (${descriptor.name}) loaded.")
-                loadedPlugins += instance
             }
         }
 
